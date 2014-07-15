24d0f364da6adbc488c1b40b560c551d4fa67a56
wInevitable <williampnoll@gmail.com>
2014-07-15 14:17:34 -0400

    Rename to Collision

--- app/assets/javascripts/views/starTrekShow.js
+++
@@ -1,9 +0,0 @@
-wInevitable.Views.StarTrek = Backbone.View.extend({
-  template: JST['star_trek'],
-
-  render: function() {
-    var renderedContent = this.template();
-    this.$el.html(renderedContent);
-    return this;
-  }
-});
